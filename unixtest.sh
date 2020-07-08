fb8cc8b38a34f174077f7bbc8ff06a783b4a2d52
srujanch <srujankrishnach@yahoo.com>
2020-07-08 16:25:35 +0530

    t

--- unixtest.sh
+++ unixtest.sh
@@ -5,4 +5,10 @@
 
 #Print the result
 echo $sum
-./dotnet tool install --global dotnet-sonarscanner --version 4.8.0
+dotnet tool install --global dotnet-sonarscanner --version 4.8.0
+
+dotnet sonarscanner begin /k:"com.testing" 
+dotnet build /mnt/optum/cloned/_3e3b5cab-2a27-4dfc-adc5-94d62c21a42d/eShopOnWeb.sln
+dotnet sonarscanner end
+
+echo "command executed"
