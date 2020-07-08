19bd76d8a47491b294c78bf17f6893634a34e955
srujanch <srujankrishnach@yahoo.com>
2020-07-08 17:47:48 +0530

    t

--- unixtest.sh
+++ unixtest.sh
@@ -7,8 +7,8 @@
 echo $sum
 #dotnet tool install --global dotnet-sonarscanner --version 4.8.0
 
-mono /com/optum/sonar-msbuild/SonarScanner.MSBuild.exe begin /k:"com.testing" 
-mono /com/optum/sonar-msbuild/SonarQube.Scanner.MSBuild.exe /mnt/optum/cloned/_3e3b5cab-2a27-4dfc-adc5-94d62c21a42d/eShopOnWeb.sln /t:Rebuild
-mono /com/optum/sonar-msbuild/SonarScanner.MSBuild.exe end 
+/com/optum/sonar-msbuild/SonarScanner.MSBuild.exe begin /k:"com.testing" /d:sonar.host.url="http://ctc2hz1-02-s45.uhc.com:30031"
+/com/optum/sonar-msbuild/SonarQube.Scanner.MSBuild.exe /mnt/optum/cloned/_3e3b5cab-2a27-4dfc-adc5-94d62c21a42d/eShopOnWeb.sln /t:Rebuild
+/com/optum/sonar-msbuild/SonarScanner.MSBuild.exe end 
 
 echo "command executed"
